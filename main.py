3ca15bcba12c0b58c5a132f80dc32d7e0c0ce694
matteozantedeschi <zantedeschi.m23@gmail.com>
2021-06-09 09:43:40 +0200

    Twitter Watcher finished

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from watchman.reddit_watcher import *
 from watchman.twitter_watcher import *
+from datetime import datetime, timedelta
 
-# REDDIT
+### REDDIT
 # Reddit Watcher init
 reddit_watcher = RedditWatcher(personal_use_script='uyW2YVoyOR9WmQ', token='hnvRZFfSWC4ZZ6m3c8rz8n9L7-5CDg', username='LeLoMAu', password='Znajzh9lREgU')
 # Get 10000 new posts from reddit communities
-df_new_posts = reddit_watcher.get_new_posts(communities=['wallstreetbets', 'finance', 'StockMarket'], how_many_posts=10000)
+df_new_posts = reddit_watcher.get_new_posts(communities=['wallstreetbets', 'finance', 'StockMarket'], how_many_posts=100)
 # Get 100 new posts from reddit communities
 df_hot_posts = reddit_watcher.get_hot_posts(communities=['wallstreetbets', 'finance', 'StockMarket'])
 
-# TWITTER
+### TWITTER
 # Twitter Watcher init
 twitter_watcher = TwitterWatcher(bearer_token='AAAAAAAAAAAAAAAAAAAAABTyQAEAAAAAnUZy8U%2FGxnBSlssbEWZWHutNolQ%3DAk8TgQnMWAxKHMxuN5UClDwhLEKoOAfFYsn9qRQEZv3HYONfx4')
-twitter_watcher.make_query(hashtags=['#stocks', '#stockmarket', '#investing', '#trading', '#finance', '#investment', '#wallstreet', '#StocksToWatch', '#StocksToBuy', '#stocksinfocus', '#stonks'])
-df_tweets = twitter_watcher.get_results()
+df_tweets = twitter_watcher.get_results(
+    hashtags=['#stocks', '#stockmarket', '#investing', '#trading', '#finance', '#investment', '#wallstreet', '#StocksToWatch', '#StocksToBuy', '#stocksinfocus', '#stonks'],
+    start_time=(datetime.now() - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+    max_results_per_page=100,
+    max_results=1000
+)
--- watchman/twitter_watcher.py
+++ watchman/twitter_watcher.py
@@ -1,21 +1,36 @@
 import logging
 import requests
 import pandas as pd
+import time
 
 logger = logging.getLogger(__name__)
 
 
 class TwitterWatcher:
+    """
+    Class to get Tweets from Twitter.
+    It enables to authenticate to the Twitter API and iteratively perform query and get results.
+    Twitter Developer portal: https://developer.twitter.com/en/portal/dashboard
+
+    Max 500.000 tweets/month
+    Max 450 requests/15 min per app auth
+    Max 180 requests/15 min per user auth
+    """
     def __init__(self, bearer_token):
         self.bearer_token = bearer_token
         self.headers = {"Authorization": "Bearer {}".format(self.bearer_token)}
         self.url = None
 
-    def make_query(self, hashtags):
+    def _make_query(self, hashtags: list, start_time=None, end_time=None, max_results_per_page=100, next_token=None, since_id=None):
         """
         Make a hhtp query from a list of hashtags
 
         :param hashtags: (list) the list of hashtags.
+        :param start_time: (str) the start time of the search. If not specified is 7 days ago.
+        :param end_time: (str) the end time of the search. If not specified is 30 seconds ago.
+        :param max_results_per_page: (int=100) max number of Tweets returned per response.
+        :param next_token: (str) the token to include in the query to get the next page of results.
+        :param since_id: (int) the id of the last tweet got.
         :return: No return.
         """
 
@@ -30,12 +45,24 @@ class TwitterWatcher:
         # Define expansion & fields
         user_expansion = "expansions=author_id"
         user_fields = "user.fields=public_metrics"
-        self.url = "https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}&{user_expansion}&{user_fields}".format(
-            query=query, tweet_fields=tweet_fields, user_expansion=user_expansion, user_fields=user_fields)
+        self.url = "https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}&{user_expansion}&{user_fields}&max_results={max_results}".format(
+            query=query, tweet_fields=tweet_fields, user_expansion=user_expansion, user_fields=user_fields, max_results=max_results_per_page)
+        # Add start_time to the query if specified
+        if start_time is not None:
+            self.url = self.url + '&start_time={start_time}'.format(start_time=start_time)
+        # Add end_time to the query if specified
+        if end_time is not None:
+            self.url = self.url + '&end_time={end_time}'.format(end_time=end_time)
+        # Add next_token to the query if specified
+        if next_token is not None:
+            self.url = self.url + '&next_token={next_token}'.format(next_token=next_token)
+        # Add since_id to the query if specified
+        if since_id is not None:
+            self.url = self.url + '&since_id={since_id}'.format(since_id=since_id)
 
         logger.info("Query made.")
 
-    def get_results(self):
+    def _get_page_results(self):
         """
         Submit the url and get the results.
 
@@ -50,11 +77,45 @@ class TwitterWatcher:
             logger.info(f'Twitter Response Status Code: {response.status_code}')
             if response.status_code != 200:
                 raise Exception(response.status_code, response.text)
-            # Return the response as json
+            # Return the results as pd.DataFrame and metadata as dict
             logger.info("Url submitted and results obtained.")
-            return self._df_from_response(response)
+            return self._df_from_response(response), response.json()['meta']
         else:
             logger.warning('Please make a query before submitting.')
+
+    def get_results(self, hashtags: list, start_time=None, end_time=None, max_results_per_page=100, max_results=15000):
+        """
+        Iteratively make http query from a list of hashtags and get results.
+        The results are then stored in a pd.DataFrame which is finally returned.
+
+        :param hashtags: (list) the list of hashtags.
+        :param start_time: (str) the start time of the search. If not specified is 7 days ago.
+        :param end_time: (str) the end time of the search. If not specified is 30 seconds ago.
+        :param max_results_per_page: (int=100) max number of Tweets returned per response.
+        :param max_results: (int=15000) max number of daily Tweets returned.
+        :return: A pd.DataFrame with all the Tweets.
+        """
+
+        # Init DataFrame with all results
+        df_results = pd.DataFrame()
+
+        # Start with the first query
+        self._make_query(hashtags=hashtags, max_results_per_page=max_results_per_page, start_time=start_time, end_time=end_time)
+        df_page_results, meta_dict = self._get_page_results()
+        df_results = df_results.append(df_page_results)
+
+        # Iterate until 15000 tweets are reached or all pages are got
+        while df_results.shape[0] < max_results and 'next_token' in meta_dict.keys():
+            self._make_query(hashtags=hashtags, max_results_per_page=max_results_per_page, start_time=start_time, end_time=end_time, next_token=meta_dict['next_token'])
+            df_page_results, meta_dict = self._get_page_results()
+            df_results = df_results.append(df_page_results)
+
+            logger.info("Tweets count: {tweets_count}".format(tweets_count=df_results.shape[0]))
+
+            # Sleep 5 seconds
+            time.sleep(5)
+
+        return df_results
 
     @staticmethod
     def _df_from_response(res: requests.Response):
